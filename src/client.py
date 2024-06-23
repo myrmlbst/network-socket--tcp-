38c536e3a933b14d57e784e2c736b6355182d7cd
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-06-23 20:15:17 +0300

    decoded test message

--- src/client.py
+++ src/client.py
@@ -7,4 +7,4 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((HOST, PORT))
 
 socket.send("Test message").encode('utf-8')
-print(socket.recv(1024))
+print(socket.recv(1024).decode('utf-8'))
