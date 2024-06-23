f5dbe993d1ced4b9acdf2186b1b74b6b2865de2b
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-06-23 20:05:28 +0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,1 +1,24 @@
 import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 1020
+
+# bind the socket to a host and port
+server.bind((HOST, PORT))
+
+# listen for incoming connections connections
+server.listen(5) # 5 unaccepted connections are allowed before rejecting new ones
+
+while True:
+    communication_socket, address = server.accept()
+    print(f"Connected to {address}")
+    message = communication_socket.recv(1024).decode('uft-8')
+    print(f"Client message: {message}")
+
+    # inform the other client that their message was sent
+    communication_socket.send(f"Message recieved.".encode('uft-8'))
+
+    communication_socket.close()
+    print(f"Connection with {address} has ended.")
