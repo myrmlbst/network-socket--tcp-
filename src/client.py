641da8d78a07b3aeeec40c5d7b6992a70e744a17
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-06-23 20:14:01 +0300

    Create client.py

---
+++ src/client.py
@@ -0,0 +1,10 @@
+import socket
+
+HOST = 'IP_ADDRESS'
+PORT = 1020
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, PORT))
+
+socket.send("Test message").encode('utf-8')
+print(socket.recv(1024))
